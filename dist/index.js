c3edb7e57edf7d71e16a67fa6f368e4c5b50855c
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-03-14 10:52:57 +0530

    work in progress

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,23 @@
 const express = require("express");
 const app = express();
 const { join } = require('node:path');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 const PORT = 3000;
+const server = createServer(app);
+const io = new Server(server);
+// const io = require('socket.io').
 app.get("/", (req, res) => {
     // res.send("App is up and running")
     res.sendFile(join(__dirname, '../index.html'));
 });
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    // console.log(socket);
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+server.listen(PORT, () => {
     console.log(`App is running on PORT ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 const express = require("express")
 const app =  express()
 const { join } = require('node:path');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
 const PORT = 3000;
+const server = createServer(app);
+const io = new Server(server)
+// const io = require('socket.io').
 
 app.get("/" , (req : any , res : any) => {
     // res.send("App is up and running")
-    res.sendFile(join(__dirname, '../index.html'));
+    res.sendFile(join(__dirname , '../index.html'));
 })
 
-app.listen(PORT , () => {
-    console.log(`App is running on PORT ${PORT}` );
+io.on('connection', (socket:any) => {
+    // console.log(socket);
+    
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+server.listen(PORT , () => {
+    console.log(`App is running on PORT ${PORT}`);
 })
+
